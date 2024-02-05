6e8b2a7af18d9532b0d36fc0eac42ffd50f12928
oliver-lister <oliverlister98@gmail.com>
2024-02-05 17:23:25 +1100

    init mongodb api

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   parser: "@typescript-eslint/parser",
   extends: ["plugin:prettier/recommended", "prettier", "eslint:recommended"],
   plugins: ["@typescript-eslint"],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,33 @@
-import "dotenv/config";
-import jwt from "jsonwebtoken";
-import multer from "multer";
-import express from "express";
-import path from "path";
-import cors from "cors";
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(cors());
+
+// Database Connection With MongoDB
+if (process.env.MONGODB) {
+  mongoose.connect(process.env.MONGODB);
+}
+
+// API Creation
+app.get('/', (req, res) => {
+  res.send('Express App Is Running');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log('Server running on port ' + port);
 });
+
+app.listen(port).on('error', (err) => {
+  console.log('Error: ' + err);
+});
+
+// https://www.youtube.com/watch?v=y99YgaQjgx4&t=18379s 5:15:31
