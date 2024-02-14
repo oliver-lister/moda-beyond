7d29913a3955e837e6536cf902e95bd6cca92e64
oliver-lister <oliverlister98@gmail.com>
2024-02-14 11:21:46 +1100

    added yup schema to mantine useform

--- admin/src/routes/AddProduct/AddProduct.tsx
+++ admin/src/routes/AddProduct/AddProduct.tsx
@@ -14,9 +14,12 @@ import {
   Button,
   Stack,
 } from "@mantine/core";
-import { useForm } from "@mantine/form";
 import { IconUpload } from "@tabler/icons-react";
 import styles from "./addproduct.module.css";
+
+import { yupResolver } from "mantine-form-yup-resolver";
+import * as yup from "yup";
+import { useForm } from "@mantine/form";
 
 interface ProductProps {
   name: string;
@@ -29,6 +32,23 @@ interface ProductProps {
   images: File[];
 }
 
+const schema = yup.object().shape({
+  name: yup.string().required("Please enter a name"),
+  brand: yup.string().required("Please enter a brand"),
+  category: yup.string().required("Please select a category"),
+  availableSizes: yup
+    .array()
+    .of(yup.string().min(1))
+    .required("Please select at least one size"),
+  description: yup.string(),
+  material: yup.string(),
+  price: yup.number(),
+  images: yup
+    .array()
+    .of(yup.mixed())
+    .required("Please upload at least one image"),
+});
+
 const AddProduct = () => {
   const form = useForm({
     initialValues: {
@@ -40,8 +60,8 @@ const AddProduct = () => {
       material: "",
       price: 50,
       images: [],
-    } as ProductProps,
-    validate: {},
+    },
+    validate: yupResolver(schema),
   });
 
   const handleSubmit = async (values: ProductProps) => {
@@ -100,20 +120,23 @@ const AddProduct = () => {
   return (
     <>
       <h2 className={styles.heading}>Add Product</h2>
-      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+      <form
+        onSubmit={
+          (console.log(form.errors),
+          form.onSubmit((values) => handleSubmit(values)))
+        }
+      >
         <Stack>
           <Fieldset>
             <SimpleGrid cols={{ lg: 1, xl: 2 }}>
               <TextInput
                 label="Product Name"
                 description="Type in the product's name."
-                placeholder="Flower Waistcoat"
                 {...form.getInputProps("name")}
               />
               <Autocomplete
                 label="Brand"
                 description="Begin searching and pick an existing brand or create a new one."
-                placeholder="ChicFashion"
                 data={["Nike", "DC", "New Balance", "Universal"]}
                 {...form.getInputProps("brand")}
               />
@@ -126,21 +149,18 @@ const AddProduct = () => {
               <MultiSelect
                 label="Available Sizes"
                 description="Select all available sizes."
-                placeholder="Pick sizes"
                 data={["INTL S", "INTL M", "INTL L", "INTL XL"]}
                 {...form.getInputProps("availableSizes")}
               />
               <Textarea
                 label="Description"
                 description="Product description."
-                placeholder="Mid-weight cotton-rich denim with minimal stretch. Zip fly with button fastening. Classic five-pocket design"
                 rows={8}
                 {...form.getInputProps("description")}
               />
               <TextInput
                 label="Material"
                 description="Describe the product's materials."
-                placeholder="Main: 99% Cotton & 1% Elastane"
                 {...form.getInputProps("material")}
               />
               <NumberInput
