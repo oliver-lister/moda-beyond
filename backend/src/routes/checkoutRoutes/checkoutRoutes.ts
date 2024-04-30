a7885fcca9b2ec43f068dea80ad26f65a2e0ea35
oliver-lister <oliverlister98@gmail.com>
2024-04-30 22:11:48 +1000

    more consolelogs

--- backend/src/routes/checkoutRoutes/checkoutRoutes.ts
+++ backend/src/routes/checkoutRoutes/checkoutRoutes.ts
@@ -87,12 +87,36 @@ router.get('/get-session/:sessionId', async (req: Request, res: Response) => {
 
     const session = await stripe.checkout.sessions.retrieve(sessionId!, { expand: ['line_items'] });
 
-    console.log(session);
+    console.log({
+      line_items: session.line_items.data,
+      id: session.id,
+      status: session.status,
+      customer_name: session.customer_details.name,
+      shipping_name: session.shipping.name,
+      shipping_address: session.shipping.address,
+      billing_name: session.billing_details.name,
+      billing_address: session.billing_details.address,
+      receipt_url: session.receipt_url,
+    });
+
+    const stripeSessionData = {
+      line_items: session.line_items.data,
+      id: session.id,
+      status: session.status,
+      customer_name: session.customer_details.name,
+      shipping_name: session.shipping.name,
+      shipping_address: session.shipping.address,
+      billing_name: session.billing_details.name,
+      billing_address: session.billing_details.address,
+      receipt_url: session.receipt_url,
+    };
+
+    console.log(stripeSessionData);
 
     return res.status(200).json({
       success: true,
       message: 'Checkout session fetched successfully',
-      stripeSessionData: session,
+      stripeSessionData: stripeSessionData,
     });
   } catch (err: any) {
     return res.status(500).json({ success: false, error: `Internal Server Error: ${err.message}`, errorCode: 'INTERNAL_SERVER_ERROR' });
