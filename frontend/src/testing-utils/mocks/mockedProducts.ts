fd613706f941b33575ed00827bd4693c0ac85316
oliver-lister <oliverlister98@gmail.com>
2024-05-02 19:34:42 +1000

    added in .env files to frontend github

---
+++ frontend/src/testing-utils/mocks/mockedProducts.ts
@@ -0,0 +1,87 @@
+export const mockedProducts = [
+  {
+    _id: "6600defd2aba10678e7b127c",
+    name: "Adicolor Woven Firebird Track Pants",
+    category: "men",
+    brand: "adidas",
+    availableSizes: ["INTL S", "INTL L"],
+    availableColors: [
+      {
+        label: "Black",
+        hex: "#000000",
+      },
+      {
+        label: "Butterfly Bush",
+        hex: "#674999",
+      },
+    ],
+    material:
+      "Main: 100% Polyamide (Recycled); Upper Lining: 100% Cotton; Lower Lining: 100% Polyester (Recycled)",
+    description:
+      "A seamless mix of technical prowess and street-ready design has helped to cement the status of the adidas Originals brand as an icon around the globe. With Trefoil logo tees, sweats and outerwear sitting alongside an inimitable sneaker edit, adidas Originals is the backbone of any off-duty rotation.",
+    price: 110,
+    images: [
+      "/images/6600defd2aba10678e7b127c/productImg_1711333117075_http___static.theiconic.com.au_p_adidas-originals-7616-5679302-1.webp",
+      "/images/6600defd2aba10678e7b127c/productImg_1711333117076_http___static.theiconic.com.au_p_adidas-originals-7637-5679302-2.webp",
+      "/images/6600defd2aba10678e7b127c/productImg_1711333117077_http___static.theiconic.com.au_p_adidas-originals-7647-5679302-3.webp",
+      "/images/6600defd2aba10678e7b127c/productImg_1711333117077_http___static.theiconic.com.au_p_adidas-originals-7658-5679302-4.webp",
+      "/images/6600defd2aba10678e7b127c/productImg_1711333117079_http___static.theiconic.com.au_p_adidas-originals-7668-5679302-5.webp",
+    ],
+    available: true,
+    date: new Date(),
+  },
+  {
+    _id: "660286b8d64e6c5891d1210e",
+    name: "adidas x Malbon Polo Shirt Mens",
+    category: "men",
+    brand: "adidas",
+    availableSizes: ["INTL XL", "INTL L"],
+    availableColors: [
+      {
+        label: "White",
+        hex: "#ffffff",
+      },
+    ],
+    material: "80% rec polyester/20% Polyester",
+    description:
+      'Course to clubhouse, this golf polo blends a casual vibe and fun graphics with traditional polo style. It features a Malbon "Buckets" logo embroidered on the left chest and a soft interlock knit blend that offers full-round comfort. "The Crosby Collection" is inspired by the legendary Bing Crosby Clambakes. Events that united celebrities, golfers, and musicians for a memorable day of golf and camaraderie. This limited-edition collection was created to celebrate every golfer and the community we all share. By choosing recycled, we are able to reuse materials that have already been created, which helps to reduce waste. Renewable materials choices will help us to remove our reliance on finite resources. Our products made with a blend of recycled and renewable materials feature at least 70% total of these materials.',
+    price: 149.5,
+    images: [
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592785_http___static.theiconic.com.au_p_adidas-performance-6784-5119822-1.webp",
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592786_http___static.theiconic.com.au_p_adidas-performance-6786-5119822-2.webp",
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592786_http___static.theiconic.com.au_p_adidas-performance-6788-5119822-3.webp",
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592786_http___static.theiconic.com.au_p_adidas-performance-6789-5119822-4.webp",
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592786_http___static.theiconic.com.au_p_adidas-performance-6791-5119822-5.webp",
+      "/images/660286b8d64e6c5891d1210e/productImg_1711441592788_http___static.theiconic.com.au_p_adidas-performance-6793-5119822-6.webp",
+    ],
+    available: true,
+    date: new Date(),
+    lastPrice: 150,
+  },
+  {
+    _id: "66028a01d64e6c5891d12516",
+    name: "Premium Essentials Nylon Parachute Pants",
+    category: "women",
+    brand: "adidas",
+    availableSizes: ["INTL L", "INTL XL"],
+    availableColors: [
+      {
+        label: "Black",
+        hex: "#000000",
+      },
+    ],
+    material: "Main: 100% Recycled Polyamide; Lining: 100% Recycled Polyester",
+    description:
+      "A seamless mix of technical prowess and street-ready design has helped to cement the status of the adidas Originals brand as an icon around the globe. With Trefoil logo tees, sweats and outerwear sitting alongside an inimitable sneaker edit, adidas Originals is the backbone of any off-duty rotation.",
+    price: 110,
+    images: [
+      "/images/66028a01d64e6c5891d12516/productImg_1711442433455_http___static.theiconic.com.au_p_adidas-originals-1762-0840402-1.webp",
+      "/images/66028a01d64e6c5891d12516/productImg_1711442433456_http___static.theiconic.com.au_p_adidas-originals-1774-0840402-2.webp",
+      "/images/66028a01d64e6c5891d12516/productImg_1711442433456_http___static.theiconic.com.au_p_adidas-originals-1787-0840402-3.webp",
+      "/images/66028a01d64e6c5891d12516/productImg_1711442433458_http___static.theiconic.com.au_p_adidas-originals-1799-0840402-4.webp",
+      "/images/66028a01d64e6c5891d12516/productImg_1711442433459_http___static.theiconic.com.au_p_adidas-originals-1812-0840402-5.webp",
+    ],
+    available: true,
+    date: new Date(),
+  },
+];
